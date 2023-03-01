6933e0a7609282a7ab862206bd7c0728ec5ce5ac
vominhnhut1104 <vominhnhut1104@gmail.com>
2023-03-01 09:32:21 +0700

    Bai28-29-30

--- Models/ProductEditModel.cs
+++ Models/ProductEditModel.cs
@@ -1,10 +1,19 @@
-﻿namespace ASPNetCoreForms.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPNetCoreForms.Models
 {
     public class ProductEditModel
     {
         public int ID { get; set; }
+
+        [Required(ErrorMessage ="Name is required")]
         public string? Name { get; set; }
         public decimal? Rate { get; set; }
         public int Rating { get; set; }
+
+        [EmailAddress] // sử dung DataAnnotation
+        public string Email { get; set; }
+
+        public DateTime? CreatedDate { get; set; }  
     }
 }
